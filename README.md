98900c24d8efe0dd2b033dcccf9c408ceca0a3e6
Arthur Barcellos <abll@cin.ufpe.br>
2020-03-24 21:35:41 -0300

    Carousel adicionado.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Site
-Criação de site 
--- js/script.js
+++ js/script.js
@@ -1,104 +1,50 @@
 //When click outside element
 document.querySelector('body').addEventListener('mouseup', function(event){
-  var sidepanel = document.getElementById("sidepanelbar");
-  var search = document.getElementById("searchbar");
-  if(event.target != sidepanel && event.target.parentNode != sidepanel){
-    closeNav();
-  }
-  if(event.target != search && event.target.parentNode != search){
-    closeSearchBar();
-  }
-});
-
-// When the user scrolls down 90px from the top of the document, resize the navbar's padding and the logo's height
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-    document.getElementById("topnavbar").style.padding = "50px 0px";
-    document.getElementById("logo").style.height = "70px";
-    document.getElementById("searchbar").style.top = "100px";
-  } else {
-    document.getElementById("topnavbar").style.padding = "110px 0px";
-    document.getElementById("logo").style.height = "200px";
-    document.getElementById("searchbar").style.top = "220px";
-  }
-}
-
-// Set the width of the sidebar to 0 (show it) 
-function openNav() {
-    document.getElementById("sidepanelbar").style.left = "0";
-    document.getElementById("overlay").style.display = "block";
+    var sidepanel = document.getElementById("sidepanelbar");
+    var search = document.getElementById("searchbar");
+    if(event.target != sidepanel && event.target.parentNode != sidepanel){
+      closeNav();
+    }
+    if(event.target != search && event.target.parentNode != search){
+      closeSearchBar();
+    }
+  });
+  
+  // When the user scrolls down 90px from the top of the document, resize the navbar's padding and the logo's height
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+      document.getElementById("topnavbar").style.padding = "50px 0px";
+      document.getElementById("logo").style.height = "70px";
+      document.getElementById("searchbar").style.top = "100px";
+    } else {
+      document.getElementById("topnavbar").style.padding = "110px 0px";
+      document.getElementById("logo").style.height = "200px";
+      document.getElementById("searchbar").style.top = "220px";
+    }
   }
   
-// Set the width of the sidebar to -300px (hide it)
-function closeNav() {
-  document.getElementById("sidepanelbar").style.left = "-300px";
-  document.getElementById("overlay").style.display = "none";
-}
-
-function openSearchBar(){
-  document.getElementById("searchbar").style.border = "1px solid #ccc";
-  document.getElementById("searchbar").style.height = "70px";
-  document.getElementById("searchbar").style.zIndex = "2";
-}
-
-function closeSearchBar(){
-  document.getElementById("searchbar").style.height = "0";
-  document.getElementById("searchbar").style.border = "none";
-  document.getElementById("searchbar").style.zIndex = "-1";
-}
-
-//Banner slides
-var slideIndex = 0;
-var t;
-carousel();
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function carousel() {
-  var i;
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  // Set the width of the sidebar to 0 (show it) 
+  function openNav() {
+      document.getElementById("sidepanelbar").style.left = "0";
+      document.getElementById("overlay").style.display = "block";
+    }
+    
+  // Set the width of the sidebar to -300px (hide it)
+  function closeNav() {
+    document.getElementById("sidepanelbar").style.left = "-300px";
+    document.getElementById("overlay").style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  
+  function openSearchBar(){
+    document.getElementById("searchbar").style.border = "1px solid #ccc";
+    document.getElementById("searchbar").style.height = "70px";
+    document.getElementById("searchbar").style.zIndex = "2";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }    
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";  
-  t = setTimeout(carousel, 9000); // Change image every 9 seconds
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }    
-  if (n < 1) {
-    slideIndex = slides.length;
+  
+  function closeSearchBar(){
+    document.getElementById("searchbar").style.height = "0";
+    document.getElementById("searchbar").style.border = "none";
+    document.getElementById("searchbar").style.zIndex = "-1";
   }
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  clearTimeout(t);
-  t = setTimeout(carousel, 9000);
-}
